a2f8d620d3c64f17fc2fa9c80887812d5224c0f0
Bhanu1234 <bhanukurakula07@gmail.com>
2025-09-02 17:46:38 +0530

    settings.py changed

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -40,10 +40,28 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'jobportal.urls'
 
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [BASE_DIR / 'templates' / 'core'],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#         },
+#     },
+# ]
+
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates' / 'core'],
+        'DIRS': [],  # Let Django find app templates automatically
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -55,6 +73,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'jobportal.wsgi.application'
 
