49f8c0d441591a13168c8517459df581059492ee
Bhanu1234 <bhanukurakula07@gmail.com>
2025-09-02 18:04:02 +0530

    settings.py changed

--- core/views.py
+++ core/views.py
@@ -4,15 +4,15 @@ from .models import JobPost
 
 def job_list(request):
     jobs = JobPost.objects.all().order_by('-posted_at')
-    return render(request, 'core/job_list.html', {'jobs': jobs})
+    return render(request, 'job_list.html', {'jobs': jobs})
 
 
 def job_detail(request, slug):
     job = get_object_or_404(JobPost, slug=slug)
     skills = [s.strip() for s in job.skills_required.split(',')]
-    return render(request, 'core/job_detail.html', {'job': job, 'skills': skills})
+    return render(request, 'job_detail.html', {'job': job, 'skills': skills})
 
 
 
 def contact(request):
-    return render(request, 'core/contact.html')
+    return render(request, 'contact.html')
