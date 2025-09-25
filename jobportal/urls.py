d1117cbb84169f9a1ce0ddaaae392e8a2a756d83
Bhanu1234 <bhanukurakula07@gmail.com>
2025-09-25 17:04:59 +0530

    Added sitemap and robots.txt for SEO

--- jobportal/urls.py
+++ jobportal/urls.py
@@ -4,15 +4,22 @@ from django.contrib.sitemaps.views import sitemap
 from core.sitemaps import StaticViewSitemap, JobPostSitemap  # use 'core', not 'jobs'
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
-
+# Define sitemaps
 sitemaps = {
     'static': StaticViewSitemap,
-    'jobs': JobPostSitemap,   # <-- updated name
+    'jobs': JobPostSitemap,
 }
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    
+    # Sitemap
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
+    
+    # Robots.txt
+    path("robots.txt", TemplateView.as_view(
+        template_name="robots.txt", content_type="text/plain")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
